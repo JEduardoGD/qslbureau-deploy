acb980dab2383dff99ebc50303e8c13c68c81b55
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2023-06-24 23:25:12 -0600

    force delete container

--- start_online.sh
+++ start_online.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker-compose --env-file env.properties rm
+docker-compose --env-file env.properties rm -f
 docker-compose --env-file env.properties pull
 docker-compose --env-file env.properties up
